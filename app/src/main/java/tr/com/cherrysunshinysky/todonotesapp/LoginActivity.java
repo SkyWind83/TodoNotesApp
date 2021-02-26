79209e7b37cb91ae653c4b72296a5598b454d083
skywind83 <emir.ozen@outlook.com>
2021-02-26 11:04:00 +0300

    Updates

--- app/src/main/java/tr/com/cherrysunshinysky/todonotesapp/LoginActivity.java
+++ app/src/main/java/tr/com/cherrysunshinysky/todonotesapp/LoginActivity.java
@@ -2,10 +2,12 @@ package tr.com.cherrysunshinysky.todonotesapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,17 +26,21 @@ public class LoginActivity extends AppCompatActivity {
         buttonLogin = findViewById(R.id.btn_login);
 
 
-
         View.OnClickListener clickAction = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String fullName = editTextFullName.getText().toString();
                 String userName = editTextUserName.getText().toString();
                 Log.d("LoginActivity", "onClick performed");
-                Intent intent = new Intent(LoginActivity.this, MyNotesActivity.class);
-                intent.putExtra("full_name", fullName);
-                intent.putExtra("user_name", userName);
-                startActivity(intent);
+
+                if (!TextUtils.isEmpty(userName) && !TextUtils.isEmpty(fullName)) {
+                    Intent intent = new Intent(LoginActivity.this, MyNotesActivity.class);
+                    intent.putExtra("full_name", fullName);
+                    intent.putExtra("user_name", userName);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Username and Fullname can not be empty!", Toast.LENGTH_SHORT).show();
+                }
             }
         };
         buttonLogin.setOnClickListener(clickAction);
--- app/src/main/java/tr/com/cherrysunshinysky/todonotesapp/MyNotesActivity.java
+++ app/src/main/java/tr/com/cherrysunshinysky/todonotesapp/MyNotesActivity.java
@@ -8,16 +8,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MyNotesActivity extends AppCompatActivity {
 
+    String fullName;
+    String userName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_notes);
         Intent intent = getIntent();
-        String fullName = intent.getStringExtra("full_name");
-        String userName = intent.getStringExtra("user_name");
+         fullName = intent.getStringExtra("full_name");
+         userName = intent.getStringExtra("user_name");
 
         Log.d("MyNotesActivity1", fullName);
         Log.d("MyNotesActivity2", userName);
+
+        if (fullName != null){
+            getSupportActionBar().setTitle(fullName);
+        }
 
     }
 }
